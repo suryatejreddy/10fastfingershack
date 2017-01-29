960cdf36c7fcc62f9554025620bb068a15b2f8ed
suryatejreddy <suryatej16102@iiitd.ac.in>
2017-01-29 17:35:25 +0530

    updated the last word limit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # typehack
-A small (10 minutes) hack that i wrote in python to automate my typing on 10fastfingers (:P).
+A small (10 minutes) hack that i wrote in python using selelnium to automate my typing on 10fastfingers (:P).
+PS: Wait for the page to be loaded.
+
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ driver.maximize_window()
 timer_element = driver.find_element_by_xpath('//*[@id="timer"]')
 timer = timer_element.get_attribute("innerHTML")
 input_element = driver.find_element_by_xpath('//*[@id="inputfield"]')
-for i in range(1,340):
+for i in range(1,350):
 	try:
 		current_word_element = driver.find_element_by_xpath('//*[@id="row1"]/span[' + str(i) +']' )
 		current_word = current_word_element.get_attribute("innerHTML")
